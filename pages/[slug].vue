4fc96116e004c306267d9f415935bb4d39be3006
Jack Domleo <jackdomleo7@gmail.com>
2023-01-14 17:54:46 +0000

    Show "Last updated" on privacy policy basicPage type

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -3,6 +3,9 @@
     <header>
       <h1 class="basic-page__title">{{ page.fields.title }}</h1>
     </header>
+    <p v-if="page.fields.slug === 'privacy-policy'">
+      Last updated: <time :datetime="dayjs(new Date(page.sys.updatedAt)).format('YYYY-MM-DD')">{{ dayjs(new Date(page.sys.updatedAt)).format('MMMM D, YYYY') }}</time>
+    </p>
     <div v-html="parseRichText(page.fields.body, { $img })" class="rich-text basic-page__content" />
   </article>
 </template>
@@ -10,12 +13,17 @@
 <script lang="ts" setup>
 import { parseRichText } from '@/utils/parseRichText'
 import type { ContentfulEntries } from '@/types/CMS/Entries'
+import dayjs from 'dayjs';
 
 const $img = useImage()
 const route = useRoute()
 
 const { data } = await useAsyncData((ctx) => { return ctx!.$contentful.getEntries<ContentfulEntries.BasicPage>({ content_type: 'basicPage', limit: 1, 'fields.slug': route.params.slug })})
 const page = data.value!.items[0]
+
+if (page.fields.slug === 'privacy-policy') {
+  const dayjs = await import('dayjs')
+}
 
 useHead({
   title: `${page.fields.title}`
