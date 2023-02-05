893eadfc52a2668d58587cd1080c1ad0a618fbbe
Jack Domleo <jackdomleo7@gmail.com>
2023-02-05 20:46:07 +0000

    Refactor parseRichText() to include blockquote, hyperlinks and code

--- utils/parseRichText.ts
+++ utils/parseRichText.ts
@@ -1,15 +1,39 @@
 import { documentToHtmlString, type Options } from '@contentful/rich-text-html-renderer';
-import type { Node, Document } from '@contentful/rich-text-types';
-import type { EntryFields } from 'contentful'
+import { type Node, type Document } from '@contentful/rich-text-types';
+import { decode } from 'html-entities';
+import type { EntryFields } from 'contentful';
 
 export function parseRichText(document: EntryFields.RichText | Node | Document, composables?: { $img: any }, options?: Options) {
   return documentToHtmlString(document, {
     renderNode: {
-      'embedded-asset-block': (node: Node) => `<img src="${composables?.$img(node.data.target.fields.file.url, { width: 768 }, { provider: 'contentful' })}" alt="${node.data.target.fields.description}" width="768" height="403" loading="lazy" />`,
-      'table': (node: Node) => `<div class="table"><table>${documentToHtmlString(node)}</table></div>`,
+      'embedded-asset-block': (node: Node) => `<img src="${composables?.$img(node.data.target.fields.file.url, { width: 768 }, { provider: 'contentful' })}" alt="${node.data.target.fields.description}" width="768" height="403" loading="lazy" style="border-radius: var(--border-radius-standard);" />`,
+      'table': (node: Node) => `<div class="table"><table>${parseRichText(node, composables)}</table></div>`,
+      'hyperlink': (node) => {
+        return `<a href="${node.data.uri}" ${node.data.uri.startsWith('/') || node.data.uri.includes('https://untoldjourneys.co.uk') ? '' : 'rel="nofollow noopener"'}>${node.content[0].value}</a>`
+      },
+      'blockquote': (node: Node) => `<div class="quote"><blockquote>${parseRichText(node, composables, {
+        renderNode: {
+          'paragraph': (node: Node) => {
+            if (node.content[0].value.startsWith('-')) {
+              return `<cite>${parseRichText(node, composables)}</cite>`
+            }
+            else {
+              return `<p>${parseRichText(node, composables)}</p>`
+            }
+          }
+        }
+      })}</blockquote></div>`,
       ...options?.renderNode
     },
     renderMark: {
+      'code': (text: string) => {
+        if (text.split('\n')[0] === 'RAW_HTML') {
+          return decode(text.replace('RAW_HTML\n', ''))
+        }
+        else {
+          return `<code>${text}</code>`
+        }
+      },
       ...options?.renderMark
     }
   })
