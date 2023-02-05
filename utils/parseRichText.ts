99a5d6fb43d1186c8a7d5cca3f5c328b7de5eeca
Jack Domleo <jackdomleo7@gmail.com>
2023-02-05 20:48:35 +0000

    Open external links in a new tab

--- utils/parseRichText.ts
+++ utils/parseRichText.ts
@@ -9,7 +9,7 @@ export function parseRichText(document: EntryFields.RichText | Node | Document, 
       'embedded-asset-block': (node: Node) => `<img src="${composables?.$img(node.data.target.fields.file.url, { width: 768 }, { provider: 'contentful' })}" alt="${node.data.target.fields.description}" width="768" height="403" loading="lazy" style="border-radius: var(--border-radius-standard);" />`,
       'table': (node: Node) => `<div class="table"><table>${parseRichText(node, composables)}</table></div>`,
       'hyperlink': (node) => {
-        return `<a href="${node.data.uri}" ${node.data.uri.startsWith('/') || node.data.uri.includes('https://untoldjourneys.co.uk') ? '' : 'rel="nofollow noopener"'}>${node.content[0].value}</a>`
+        return `<a href="${node.data.uri}" ${node.data.uri.startsWith('/') || node.data.uri.includes('https://untoldjourneys.co.uk') ? '' : 'rel="nofollow noopener" target="_blank"'}>${node.content[0].value}</a>`
       },
       'blockquote': (node: Node) => `<div class="quote"><blockquote>${parseRichText(node, composables, {
         renderNode: {
