fda41a7a2ec8d13710aa203c12b6665f00ad5acf
Jack Domleo <jackdomleo7@gmail.com>
2023-03-19 10:39:14 +0000

    Remove Twitter

--- utils/socialLinks.ts
+++ utils/socialLinks.ts
@@ -5,6 +5,5 @@ export interface ISociaLink {
 }
 
 export const socialLinks: ISociaLink[] = [
-  { icon: 'twitter', text: 'Twitter', url: 'https://twitter.com/Untold__Journey' },
   { icon: 'instagram', text: 'Instagram', url: 'https://www.instagram.com/untold_journeys_blog/' }
 ]
